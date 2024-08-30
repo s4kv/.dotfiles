3eafc4db42849372d168b32a8b194788c59b27f6
s4kv <sibarguren@gmail.com>
2024-08-30 11:09:49 -0400

    polute

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -47,6 +47,7 @@
 ; (gtk_accel_path "<Actions>/ThunarStandardView/unselect-all-files" "Escape")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/copy-2" "<Primary>Insert")
 ; (gtk_accel_path "<Actions>/ThunarWindow/zoom-out-alt" "<Primary>KP_Subtract")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/161292ff1e5fe8da047178641b1f2c6d" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/cancel-search" "Escape")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/select-by-pattern" "<Primary>s")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/rename" "F2")
